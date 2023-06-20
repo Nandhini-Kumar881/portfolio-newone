7c24c0d6ac3184420b6f5c80c80eb62ab3eb7654
nandhini <nandhunandhini881@gmail.com>
2023-06-20 20:03:39 +0530

    first commit

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,39 @@
+import Proj1 from "../assets/proj1.jpg";
+import Proj2 from "../assets/proj2.jpg";
+import Proj3 from "../assets/proj3.webp";
+import Proj4 from "../assets/proj4.webp";
+import Proj5 from "../assets/proj5.jpg";
+import Proj6 from "../assets/proj6.png";
+
+export const ProjectList = [
+  {
+    name: "HTML 5",
+    image: Proj1,
+    skills : "ddd",
+  },
+  {
+    name: "CSS 3",
+    image: Proj2,
+    skills: "React,Node.js,MongoDB",
+  },
+  {
+    name: "JavaScript",
+    image: Proj3,
+    skills: "React,Node.js,MongoDB,SpotifyAPI",
+  },
+  {
+    name: "React JS",
+    image: Proj4,
+    skills: "React,Node.js,MySQL,GraphQL",
+  },
+  {
+    name: "Redux",
+    image: Proj5,
+    skills: "JavaScript,HTML,CSS",
+  },
+  {
+    name: "Bootstrap",
+    image: Proj6,
+    skills: "React Native,JavaScript,HTML,CSS",
+  },
+];
---
+++ src/pages/Experience.js
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@mui/icons-material/School";
+import WorkIcon from "@mui/icons-material/Work";
+
+function Experience() {
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2015 - 2017"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            PKD Matriculation Higher Secondary School
+          </h3>
+          <p>90.3%</p>
+          <p> High School Diploma</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2017 - 2020"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            PSG College Of Arts And Science
+          </h3>
+
+          <h4 className="vertical-timeline-element-subtitle">
+            Bachelor's Degree
+          </h4>
+          <p>73.4%</p>
+
+          <p> Computer Science</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2021 - 2022"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Programmer Trainee - Cognizant Technogy Solutions
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Bangalore
+          </h4>
+          <p>Learned MERN Stack Development and applied those technologies to develop a Flower boutique e-commerce website.</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2021 - present"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Programmer Analyst - Cognizant Technogy Solutions
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Bangalore
+          </h4>
+          <p>
+            Worked in WALGREENS FINDCARE Immunization Project as a Front-End developer 
+            and effectively translated client requirements into application designs and system requirements.
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
+}
+
+export default Experience;
